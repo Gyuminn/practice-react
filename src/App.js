01212a75e4d37f7a4d8c1853535dc3c03a09e902
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-12-20 19:09:10 +0900

    isRequired: 필수 propTypes 검증

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MyComponents from "./MyComponents";
 
 const App = () => {
-  return <MyComponents name={3} />;
+  return <MyComponents name="React" favoriteNumber={3} />;
 };
 
 export default App;
--- src/MyComponents.js
+++ src/MyComponents.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const MyComponents = (props) => {
-  console.log(props);
-  return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
+const MyComponents = ({ name, favoriteNumber }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name}입니다.
+      <br />
+      제가 제일 좋아하는 숫자는 {favoriteNumber} 입니다.
+    </div>
+  );
 };
 
 MyComponents.defaultProps = {
@@ -12,5 +17,6 @@ MyComponents.defaultProps = {
 
 MyComponents.propTypes = {
   name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 export default MyComponents;
