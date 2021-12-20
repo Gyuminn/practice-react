79fc51fd692ccde05e59d2af666385c0cc2e2f25
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-12-20 19:51:16 +0900

    useState 복습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import EventPractice from "./EventPractice";
 import MyComponents from "./MyComponents";
+import Say from "./Say";
 
 const App = () => {
-  return <MyComponents name="React" favoriteNumber={3} />;
+  return (
+    <>
+      <MyComponents name="React" favoriteNumber={3} />
+      <Say />
+      <EventPractice />
+    </>
+  );
 };
 
 export default App;
