0c37a8905dd6124bf826736fbd18f143fc61fee3
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-12-20 20:42:44 +0900

    useState 안에 객체로 한꺼번에 관리하기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 
 const EventPractice = () => {
-  const [username, setUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  }); // 객체로 한꺼번에 관리
+
+  const { username, message } = form; // 구조분해 할당
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    console.log(e.target.value);
+    console.log(nextForm);
+    setForm(nextForm);
+  };
+
   const onClick = () => {
     alert(username + ": " + message);
-    setUsername("");
-    setMessage("");
+    setForm({
+      username: "",
+      message: "",
+    });
   };
 
   const onKeyPress = (e) => {
@@ -16,6 +30,7 @@ const EventPractice = () => {
       onClick();
     }
   };
+
   return (
     <div>
       <h1>이벤트 연습</h1>
@@ -23,14 +38,15 @@ const EventPractice = () => {
         type="text"
         name="username"
         placeholder="사용자명"
-        onChange={onChangeUsername}
+        // value={username}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
-        value={message}
-        onChange={onChangeMessage}
+        // value={message}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}></button>
