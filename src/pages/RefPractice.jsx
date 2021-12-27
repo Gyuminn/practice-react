ad834f6b763a02ffc924538a73c4ebf3692f2c24
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-12-27 19:08:17 +0900

    Comment: DOM에 직접적으로 접근하는 경우 주석 추가

--- src/pages/RefPractice.jsx
+++ src/pages/RefPractice.jsx
@@ -1,6 +1,11 @@
 import React, { useRef } from "react";
 
 const RefPractice = () => {
+  //DOM을 꼭 사용해야 하는 상황
+  //1. 특정 input에 포커스 주기
+  //2. 스크롤 박스 조작하기
+  //3. Canvas 요소에 그림 그리기 등
+  // 이 밖에도 여러 경우가 있고, 어쩔 수 없이 DOM에 직접적으로 접근해야 할 때 ref를 사용한다.
   const inputEl = useRef(null);
   const onButtonClick = () => {
     inputEl.current.focus();
