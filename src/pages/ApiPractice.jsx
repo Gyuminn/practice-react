fa003ac1ddd8197c70a1bf8ad6ad3b11dda3bff5
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-12-30 15:01:36 +0900

    feat: axios.get 구현

--- src/pages/ApiPractice.jsx
+++ src/pages/ApiPractice.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const ApiPractice = () => {
-  return <div></div>;
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setError(null);
+        setUsers(null);
+
+        //loading 상태는 true로 변경
+        setLoading(true);
+        const response = await axios.get(
+          "http://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다!</div>;
+  if (!users) return null;
+  return (
+    <div>
+      {users.map((user, idx) => (
+        <li key={idx}>{user.username}</li>
+      ))}
+    </div>
+  );
 };
 
 export default ApiPractice;
