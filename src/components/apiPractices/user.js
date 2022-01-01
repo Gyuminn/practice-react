da57cb643a01fd360e4eebf4254bdfbfdbee59b5
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-01-01 20:59:59 +0900

    Design: 주석 추가

--- src/components/apiPractices/user.js
+++ src/components/apiPractices/user.js
@@ -9,7 +9,8 @@ const getUser = async () => {
   return response.data;
 };
 
-
 export const User = ({ id }) => {
   const [state] = useAsync(() => getUser(id));
+  // 어떻게 id를 넘길 수 있는지 고민을 해보자.
+  // 구조분해 할당에 대해서 더 공부하기
 };
