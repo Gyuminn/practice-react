26b8d8343cf0d9aa276e48683f2badb96a405987
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-03-08 17:59:22 +0900

    feat: Coin Interface 설정

--- src/components/cryptoTrackers/Coin.tsx
+++ src/components/cryptoTrackers/Coin.tsx
@@ -1,13 +1,138 @@
 //All Coins
 //:id -> /btc -> detail
 
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 //btc/information
 //btc/chart
 
-export default function Coin() {
-  const { coinId } = useParams();
-  console.log(coinId);
-  return <h1>Coin : {coinId}</h1>;
+interface RouterState {
+  name: string;
 }
+
+interface ITag {
+  coin_counter: number;
+  ico_counter: number;
+  id: string;
+  name: string;
+}
+
+interface ITeam {
+  id: string;
+  name: string;
+  position: string;
+}
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  tags: ITag[]; // 사용안하긴 함. 근데 객체로 이루어진 배열이기 때문에 한번 해봄.브라우저에서 불러왔을 때는 Object였어서 수작업해줌.
+  team: ITeam[]; // 얘도 마찬가지.
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object; //안씀.
+  links_extended: object; //안씀.
+  whitepaper: object; //안씀.
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: 68692.137036932;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+export default function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams();
+  const location = useLocation();
+  const state = location.state as RouterState;
+  // const {state} = useLocation<RouterState>(); -> 이 방식은 v6부터 제네릭을 지원하지 않기때문에 불가능하다.
+
+  const [info, setInfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceData>();
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
+}
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
