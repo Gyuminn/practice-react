3926616fa2305255f4b1d773b239564a68074751
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-03 18:33:37 +0900

    practice react-query caching % divide api

--- src/api.tsx
+++ src/api.tsx
@@ -1,5 +1,19 @@
+const BASE_URL = "https://api.coinpaprika.com/v1";
+
 export const fetchCoins = async () => {
-  return await await fetch("https://api.coinpaprika.com/v1/coins").then(
-    (response) => response.json()
+  return await await fetch(`${BASE_URL}/coins`).then((response) =>
+    response.json()
+  );
+};
+
+export const fetchCoinInfo = async (coinId: string | undefined) => {
+  return await await fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
+    response.json()
+  );
+};
+
+export const fetchCoinTickers = async (coinId: string | undefined) => {
+  return await await fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
   );
 };
--- src/components/cryptoTrackers/Coin.tsx
+++ src/components/cryptoTrackers/Coin.tsx
@@ -2,6 +2,7 @@
 //:id -> /btc -> detail
 
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import {
   Link,
   Outlet,
@@ -12,6 +13,7 @@ import {
   useParams,
 } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoinInfo, fetchCoinTickers } from "../../api";
 import Chart from "./Chart";
 import Price from "./Price";
 
@@ -94,35 +96,45 @@ interface IPriceData {
   };
 }
 export default function Coin() {
-  const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
   const location = useLocation();
   const state = location.state as RouterState;
   // const {state} = useLocation<RouterState>(); -> 이 방식은 v6부터 제네릭을 지원하지 않기때문에 불가능하다.
-
-  const [info, setInfo] = useState<IInfoData>();
-  const [priceInfo, setPriceInfo] = useState<IPriceData>();
-
   const priceMatch = useMatch("/:coinId/price");
   const chartMatch = useMatch("/:coinId/chart");
-  useEffect(() => {
-    (async () => {
-      const infoData = await (
-        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-      ).json();
-      const priceData = await (
-        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-      ).json();
-      setInfo(infoData);
-      setPriceInfo(priceData);
-      setLoading(false);
-    })();
-  }, [coinId]);
+  const { isLoading: infoLoading, data: infoData } = useQuery<IInfoData>(
+    ["info", coinId],
+    () => fetchCoinInfo(coinId)
+  );
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<IPriceData>(
+    ["tickers", coinId],
+    () => fetchCoinTickers(coinId)
+  );
+
+  // const [loading, setLoading] = useState(true);
+  // const [info, setInfo] = useState<IInfoData>();
+  // const [priceInfo, setPriceInfo] = useState<IPriceData>();
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const infoData = await (
+  //       await fetch(`/coins/${coinId}`)
+  //     ).json();
+  //     const priceData = await (
+  //       await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+  //     ).json();
+  //     setInfo(infoData);
+  //     setPriceInfo(priceData);
+  //     setLoading(false);
+  //   })();
+  // }, [coinId]);
+
+  const loading = infoLoading || tickersLoading;
   return (
     <Container>
       <Header>
         <Title>
-          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
         </Title>
       </Header>
       {loading ? (
@@ -132,26 +144,26 @@ export default function Coin() {
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{info?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${info?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Open Source:</span>
-              <span>{info?.open_source ? "Yes" : "No"}</span>
+              <span>{infoData?.open_source ? "Yes" : "No"}</span>
             </OverviewItem>
           </Overview>
-          <Description>{info?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Suply:</span>
-              <span>{priceInfo?.total_supply}</span>
+              <span>{tickersData?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
+              <span>{tickersData?.max_supply}</span>
             </OverviewItem>
           </Overview>
           <Tabs>
