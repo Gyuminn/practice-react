6c536d90606eefceee3eb04c86865fe6c7a5c273
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-17 09:00:42 +0900

    add the ReactQueryDevTools

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Router from "./router";
 import GlobalStyle from "./styles/globalstyles";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </>
   );
 };
