42f74ee63dd92efdcdfe93bc6ecc2360496423b8
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-20 01:15:52 +0900

    yarn add react-apexcharts

--- src/components/cryptoTrackers/Chart.tsx
+++ src/components/cryptoTrackers/Chart.tsx
@@ -1,16 +1,46 @@
 import { useQuery } from "react-query";
 import { useOutletContext } from "react-router-dom";
 import { fetchCoinHistory } from "../../api";
-
+import ApexChart from "react-apexcharts";
+interface IHistorical {
+  close: string;
+  high: string;
+  low: string;
+  market_cap: number;
+  open: string;
+  time_close: number;
+  time_open: number;
+  volume: string;
+}
 interface ChartProps {
   coinId: string;
 }
 
 export default function Chart() {
   const { coinId } = useOutletContext<ChartProps>();
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
   console.log(isLoading, data);
-  return <h1>{coinId}</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            { name: "hello", data: [1, 2, 3, 4, 5, 6] },
+            { name: "sales", data: [15, 17, 15, 18, 56] },
+          ]}
+          options={{
+            chart: {
+              height: 500,
+              width: 500,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
