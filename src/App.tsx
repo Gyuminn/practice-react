8d4d3da653bd5b489c750ca226d5bc0ecd6d63af
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-21 23:03:47 +0900

    add button for dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import Router from "./router";
 import GlobalStyle from "./styles/globalstyles";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./styles/theme";
+import { useState } from "react";
 
 const App = () => {
+  const [isDark, setIsDark] = useState(false);
+  const toggleDark = () => setIsDark((prev) => !prev);
   return (
     <>
-      <GlobalStyle />
-      <Router />
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <button onClick={toggleDark}>Toggle Mode</button>
+        <GlobalStyle />
+        <Router />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </ThemeProvider>
     </>
   );
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
-    bgColor: "#2d3436",
-    textColor: "#dfe6e9",
-    accentColor: "#00cec9",
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "black",
+  accentColor: "#9c88ff",
+  
 }
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "black",
+  accentColor: "#9c88ff",
+}
+
