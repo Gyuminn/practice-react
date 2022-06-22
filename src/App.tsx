3c53d8fa33f234cdee8076726989f9568775180a
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-23 02:10:19 +0900

    yarn add recoil & remove isDark state

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,11 @@ import GlobalStyle from "./styles/globalstyles";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./styles/theme";
-import { useState } from "react";
 
 const App = () => {
-  const [isDark, setIsDark] = useState(false);
-  const toggleDark = () => setIsDark((prev) => !prev);
   return (
     <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <button onClick={toggleDark}>Toggle Mode</button>
+      <ThemeProvider theme={false ? darkTheme : lightTheme}>
         <GlobalStyle />
         <Router />
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
